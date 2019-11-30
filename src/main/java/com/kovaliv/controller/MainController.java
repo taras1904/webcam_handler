eebca191ad1d1d93c58a6f3d39a886d52e49a0b4
taras <taras19041@gmail.com>
2019-11-30 20:56:58 +0200

    White and Black filter

--- src/main/java/com/kovaliv/controller/MainController.java
+++ src/main/java/com/kovaliv/controller/MainController.java
@@ -1,20 +1,27 @@
 package com.kovaliv.controller;
 
+import com.kovaliv.threads.VideoThread;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class MainController {
 
     @FXML
     private Button startButton;
-
     @FXML
     private Button saveButton;
+    @FXML
+    private ImageView mainScreen;
+    @FXML
+    private ImageView secondScreen;
 
     @FXML
     private void start(ActionEvent event) {
         startButton.setText("Started");
+        VideoThread videoThread = new VideoThread(mainScreen, secondScreen);
+        videoThread.start();
     }
 
 
---
+++ src/main/java/com/kovaliv/imageHandlers/WhiteAndBlackFilter.java
@@ -0,0 +1,37 @@
+package com.kovaliv.imageHandlers;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class WhiteAndBlackFilter {
+    BufferedImage image;
+    Dimension dimension;
+
+    public WhiteAndBlackFilter(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    public BufferedImage filter(BufferedImage bufferedImage) {
+        image = bufferedImage;
+        for (int i = 0; i < dimension.width; i++) {
+            for (int j = 0; j < dimension.height; j++) {
+                Pixel pixel = new Pixel(image.getRGB(i, j));
+                if (isBlack(pixel)) {
+                    image.setRGB(i, j, Color.BLACK.getRGB());
+                } else {
+                    image.setRGB(i, j, Color.WHITE.getRGB());
+                }
+            }
+        }
+        return image;
+    }
+
+    private boolean isBlack(Pixel pixel) {
+        System.out.println(pixel.getSum());
+        return pixel.getSum() < 384;
+    }
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
+}
